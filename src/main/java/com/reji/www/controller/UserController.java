e32902a1b916249bc46775149cb6818eff663e5c
hpx <2976633013@qq.com>
2022-07-20 19:28:35 +0800

    redis cache login

--- src/main/java/com/reji/www/controller/UserController.java
+++ src/main/java/com/reji/www/controller/UserController.java
@@ -9,6 +9,7 @@ import com.reji.www.service.UserService;
 import com.reji.www.utils.SMSUtils;
 import com.reji.www.utils.ValidateCodeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -23,23 +25,28 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @PostMapping("/sendMsg")
-    public void sendMsg(HttpServletRequest request, @RequestBody User user) throws ClientException {
+    public void sendMsg(@RequestBody User user) throws ClientException {
         String phone = user.getPhone();
         String s = String.valueOf(ValidateCodeUtils.generateValidateCode(6));
 
 //        SMSUtils.sendMessage(phone,s);
 
-        request.getSession().setAttribute("code",s);
+//        request.getSession().setAttribute("code",s);
+        redisTemplate.opsForValue().set("code",s,5, TimeUnit.MINUTES);
 
         System.out.println("短信验证码==============="+s);
 
     }
 
     @PostMapping("/login")
-    public Result login(HttpServletRequest request, @RequestBody Map map){
+    public Result login(HttpServletRequest request,@RequestBody Map map){
         String code = (String) map.get("code");
-        String codeSession = (String) request.getSession().getAttribute("code");
+//        String codeSession = (String) request.getSession().getAttribute("code");
+        String codeSession = (String) redisTemplate.opsForValue().get("code");
 
         if (!code.equals(codeSession)){
             return Result.error("验证码错误");
